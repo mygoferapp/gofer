18674791885f264019f292fb045cd7883c66cbd6
Ravindra Jarajapu <mygoferapp@gmail.com>
2016-08-17 20:54:03 -0700

    refactor

--- src/main/java/com/gofer/services/HelloWorld.java
+++ src/main/java/com/gofer/services/HelloWorld.java
@@ -5,8 +5,6 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
-import com.gofer.services.uber.HelloResponse;
-
 
 @Path("/hello")
 public class HelloWorld {
